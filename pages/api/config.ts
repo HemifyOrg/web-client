5318a10fa02cd9dd5cc70a728b2acc774cf89bc7
Divine Ikhuoria <divuzki@gmail.com>
2023-07-07 20:40:20 +0100

    Updated UI & Fixed Edge Problem

--- pages/api/config.ts
+++ pages/api/config.ts
@@ -1,11 +1,17 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-export const runtime = "nodejs"; // 'nodejs' (default) | 'edge'
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    name: 'Hemify',
-    infuraId: process.env.INFURA_KEY,
-    chainId: process.env.CHAIN_ID,
-    networkName: process.env.NETWORK_NAME,
-    usdtContractAddress: '0xdac17f958d2ee523a2206206994597c13d831ec7',
-  })
+import { NextApiRequest } from "next";
+import { NextResponse } from "next/server";
+export const runtime = "edge"; // 'nodejs' (default) | 'edge'
+export default async function GET(req: NextApiRequest) {
+  return NextResponse.json(
+    {
+      name: "Hemify",
+      infuraId: process.env.INFURA_KEY,
+      chainId: process.env.CHAIN_ID,
+      networkName: process.env.NETWORK_NAME,
+      usdtContractAddress: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+    },
+    {
+      status: 200,
+    }
+  );
 }
