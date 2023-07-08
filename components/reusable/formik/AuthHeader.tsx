a019cef3840f4e4d6e1572a3acaa5ba3bf6361fa
Divine Ikhuoria <divuzki@gmail.com>
2023-07-08 12:39:09 +0100

    Added `Login Page`

--- components/reusable/formik/AuthHeader.tsx
+++ components/reusable/formik/AuthHeader.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { useRouter } from "next/router";
 
-const AuthHeader = ({type}: {type: "login" | "signup"}) => {
+const AuthHeader = ({ type }: { type: "login" | "signup" }) => {
+  const router = useRouter();
+
+  const handleRedirect = (type: string) => {
+    router.push("/" + type);
+  };
   return (
     <div className="py-2 my-2 flex flex-col gap-1 justify-center items-center w-full">
       <div className="flex flex-col relative">
         <div className="flex gap-4">
           <button
+            onClick={() => handleRedirect("login")}
             type="button"
             className="p-1 font-semibold text-gray-700 text-lg"
           >
@@ -14,6 +21,7 @@ const AuthHeader = ({type}: {type: "login" | "signup"}) => {
           </button>
           <button
             type="button"
+            onClick={() => handleRedirect("login")}
             className="p-1 font-semibold text-gray-700 text-lg"
           >
             SignUp
@@ -21,9 +29,13 @@ const AuthHeader = ({type}: {type: "login" | "signup"}) => {
         </div>
         <motion.div
           initial={{ width: "0%" }}
-          animate={{ width: type === "login" ? "50%" : "50%", left: type === "login" ? "0%" : "50%" }}
+          animate={{
+            width: type === "login" ? "50%" : "50%",
+            left: type === "login" ? "0%" : "50%",
+          }}
           transition={{ duration: 0.3 }}
-         className="w-10 h-2 absolute -bottom-1 left-0 bg-themeColor rounded-full shadow-md"></motion.div>
+          className="w-10 h-2 absolute -bottom-1 left-0 bg-themeColor rounded-full shadow-md"
+        ></motion.div>
       </div>
     </div>
   );
--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -1,4 +1,81 @@
+import { AuthHeader, InputField } from "@/components/reusable/formik";
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
+
 const LoginPage = () => {
-  return <></>;
+  const SignupSchema = Yup.object().shape({
+    fullName: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    username: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    email: Yup.string().email("Invalid email").required("Required"),
+    checkedTerms: Yup.boolean().oneOf(
+      [true],
+      "You must accept the terms and conditions"
+    ),
+  });
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+  const fieldsList = [
+    {
+      name: "email",
+      label: "Email",
+      type: "email",
+      placeholder: "Enter your email address",
+      autoComplete: "email",
+    },
+    {
+      name: "password",
+      label: "Password",
+      type: "password",
+      placeholder: "Enter your password",
+      autoComplete: "new-password",
+    },
+  ];
+
+  return (
+    <main className="md:mt-32 mt-20 mb-12 p-4 gap-4 h-full flex w-full md:w-[45%] lg:w-1/3 m-auto flex-col items-center justify-start lg:justify-center">
+      <AuthHeader type="login" />
+      <Formik
+        initialValues={initialValues}
+        validationSchema={SignupSchema}
+        onSubmit={(values, actions) => {
+          console.log({ values, actions });
+          alert(JSON.stringify(values, null, 2));
+          actions.setSubmitting(false);
+        }}
+      >
+        {({ dirty }) => (
+          <>
+            <p>Kindly</p>
+            <Form
+              className={`form md:p-2 flex flex-col gap-8 overflow-hidden md:border-[0.5px] md:border-themeColor md:bg-white  w-full`}
+            >
+              {fieldsList.slice(0, 4).map((field, index) => (
+                <InputField {...field} key={index} />
+              ))}
+
+              <div className="flex gap-4 my-2 justify-center items-center w-full">
+                <button
+                  type="button"
+                  disabled={!dirty}
+                  className="bg-btnImage disabled:cursor-not-allowed disabled:opacity-50 rounded-full px-5 w-full active:scale-90 transition-all text-gray-700 font-bold py-2"
+                >
+                  Login
+                </button>
+              </div>
+            </Form>
+          </>
+        )}
+      </Formik>
+    </main>
+  );
 };
+
 export default LoginPage;
