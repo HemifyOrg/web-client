dec9abb54c5bc85e74d701aed7be70ae8cda7150
B-mitchell <mitchellonuorah77@gmail.com>
2023-07-14 15:45:50 +0100

    updates

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -5,6 +5,7 @@ const ProfilePage = () =>  {
             {/* top */}
             <div className="w-full flex flex-col">
                 <figure className="h-[500px] bg-gray-400 w-full"></figure>
+                 <h1 className="mt-10">Mitchell is here</h1>
             </div>
         </div>
     )
