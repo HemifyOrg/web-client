cddc0997ef650fb90889834d7d8cbc17b56bdf59
Divine Ikhuoria <divuzki@gmail.com>
2023-08-09 03:22:22 +0100

    Hide MVP & Show Landing Page

---
+++ components/landing/MainComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroComponent from "./HeroComponent";
+
+const MainComponent = () => {
+  return (
+    <main className="mt-12 flex flex-col h-full mb-5">
+      <HeroComponent />
+    </main>
+  );
+};
+
+export default MainComponent;
