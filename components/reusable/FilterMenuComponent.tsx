c02f1359b8756a9956ba320e111c4a52db92c04c
Divine Ikhuoria <divuzki@gmail.com>
2023-10-19 00:29:21 +0100

    Added More Functionality To FilterMenuComponent

--- components/reusable/FilterMenuComponent.tsx
+++ components/reusable/FilterMenuComponent.tsx
@@ -6,41 +6,98 @@ type Props = {
     name: string;
     icon: React.ReactNode;
   }>;
+  tabs2: Array<{
+    name: string;
+    icon: React.ReactNode;
+  }>;
   selectedTab: string;
   setSelectedTab: React.Dispatch<React.SetStateAction<string>>;
+  selectedTab2?: string;
+  setSelectedTab2?: React.Dispatch<React.SetStateAction<string>>;
   className?: string;
+  rightIcon?: React.ReactNode;
+  handleRightIconClick?: () => void;
 };
 
 const FilterMenuComponent: React.FC<Props> = ({
   tabs,
+  tabs2,
   selectedTab,
   setSelectedTab,
+  selectedTab2,
+  setSelectedTab2,
   className,
+  rightIcon,
+  handleRightIconClick,
 }) => {
   return (
     <div
-      className={
-        className ||
-        `mx-auto overflow-x-auto w-full px-4 py-2 shadow-sm rounded-full bg-white flex gap-4 items-center justify-start`
-      }
+      className={`bg-white ${
+        tabs2 && selectedTab2 && setSelectedTab2
+          ? "rounded-3xl"
+          : "rounded-full"
+      } flex relative overflow-hidden gap-1 divide-x-2s`}
     >
-      {tabs.map((tab, index) => (
-        <motion.button
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.9 }}
-          type="button"
-          onClick={() => setSelectedTab(tab.name)}
-          key={index}
-          className={`flex gap-2 items-center ${
-            selectedTab === tab.name
-              ? "bg-darkGold text-white"
-              : "hover:border-darkGold border text-gray-700 bg-secondary"
-          } px-8 py-1 rounded-full `}
+      <div className="flex flex-col w-full">
+        <div
+          className={
+            className ||
+            `mx-auto overflow-x-auto w-full px-4 py-2 shadow-sm bg-white flex gap-4 items-center justify-start`
+          }
         >
-          <span className="w-6 h-6">{tab.icon}</span>
-          <span className="text-xs capitalize">{tab.name}</span>
-        </motion.button>
-      ))}
+          {tabs.map((tab, index) => (
+            <motion.button
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.9 }}
+              type="button"
+              onClick={() => setSelectedTab(tab.name)}
+              key={index}
+              className={`flex gap-2 items-center ${
+                selectedTab === tab.name
+                  ? "bg-darkGold text-white"
+                  : "hover:border-darkGold border text-gray-700 bg-secondary"
+              } px-8 py-[2px] rounded-full `}
+            >
+              <span className="w-6 h-6">{tab.icon}</span>
+              <span className="text-xs capitalize">{tab.name}</span>
+            </motion.button>
+          ))}
+        </div>
+        {tabs2 && selectedTab2 && setSelectedTab2 && (
+          <div
+            className={
+              className ||
+              `mx-auto overflow-x-auto w-full px-4 py-2 shadow-sm bg-white flex gap-4 items-center justify-start`
+            }
+          >
+            {tabs2.map((tab, index) => (
+              <motion.button
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.9 }}
+                type="button"
+                onClick={() => setSelectedTab(tab.name)}
+                key={index}
+                className={`flex gap-2 items-center ${
+                  selectedTab === tab.name
+                    ? "bg-darkGold text-white"
+                    : "hover:border-darkGold border text-gray-700 bg-secondary"
+                } px-8 py-[2px] rounded-full `}
+              >
+                <span className="w-6 h-6">{tab.icon}</span>
+                <span className="text-xs capitalize">{tab.name}</span>
+              </motion.button>
+            ))}
+          </div>
+        )}
+      </div>
+      {rightIcon && (
+        <span
+          onClick={handleRightIconClick}
+          className="flex my-auto px-2 ml-1 justify-center items-center border-l-2 border-gray-200"
+        >
+          {rightIcon}
+        </span>
+      )}
     </div>
   );
 };
--- pages/product/wager/index.tsx
+++ pages/product/wager/index.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const WagerPage = () => {
   return (
-    <div>DuelBetPage</div>
-  )
-}
+    <div className="mt-20 flex flex-col w-full px-5 justify-center items-center">
+      <div className="w-full flex justify-end items-center p-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 20 20"
+          fill="none"
+        >
+          <path
+            d="M15 6.66663C15 5.34054 14.4732 4.06877 13.5355 3.13109C12.5979 2.19341 11.3261 1.66663 10 1.66663C8.67392 1.66663 7.40215 2.19341 6.46447 3.13109C5.52678 4.06877 5 5.34054 5 6.66663C5 9.86927 4.24643 11.816 3.56675 12.9204C3.30747 13.3418 3.65668 14.1666 4.15139 14.1666H15.8486C16.3433 14.1666 16.6925 13.3418 16.4333 12.9204C15.7536 11.816 15 9.86927 15 6.66663Z"
+            stroke="black"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M11.4419 17.5C11.2954 17.7526 11.0851 17.9622 10.8321 18.1079C10.5791 18.2537 10.2922 18.3304 10.0003 18.3304C9.70828 18.3304 9.42142 18.2537 9.1684 18.1079C8.91539 17.9622 8.7051 17.7526 8.55859 17.5"
+            stroke="black"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </div>
 
-export default WagerPage
+      <div className="flex flex-col gap-6 mt-5">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default WagerPage;
