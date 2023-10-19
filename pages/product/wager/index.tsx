c10af0d97475c8d46831ec0c1131bd49529fd7d2
Divine Ikhuoria <divuzki@gmail.com>
2023-10-19 03:06:26 +0100

    Finished Wager Page UI

--- pages/product/wager/index.tsx
+++ pages/product/wager/index.tsx
@@ -1,6 +1,94 @@
-import React from "react";
+import { useState, useEffect, Fragment } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Keyboard, Scrollbar, Pagination } from "swiper/modules";
+import { sportsList } from "@/utils";
+import { FilterMenuComponent } from "@/components/reusable";
+import { EventType, WagerType } from "@/utils/types";
+import { eventList, wagerList } from "@/utils/dummyDatas";
+import {
+  EventWagerCardComponent,
+  WagerCardComponent,
+} from "@/components/reusable/wager";
+import { Swiper as SwiperClass } from "swiper/types";
+
+const EventsSlideComponent = () => {
+  const [events, setEvents] = useState<EventType[]>(eventList);
+  const [selectedFilterBySport, setSelectedFilterBySport] = useState("all");
+  const [selectedFilterByCountry, setSelectedFilterByCountry] = useState("all");
+  const [filteredEvents, setFilteredEvents] = useState<EventType[]>([]);
+
+  useEffect(() => {
+    if (selectedFilterBySport === "all") setFilteredEvents(events);
+    else
+      setFilteredEvents(
+        events.filter((event) => event.sport.name === selectedFilterBySport)
+      );
+  }, [selectedFilterBySport]);
+  return (
+    <Fragment>
+      <div className="md:max-w-2xl max-w-md w-full mt-2 pb-4">
+        <FilterMenuComponent
+          tabs={sportsList}
+          tabs2={sportsList}
+          selectedTab={selectedFilterBySport}
+          setSelectedTab={setSelectedFilterBySport}
+          selectedTab2={selectedFilterByCountry}
+          setSelectedTab2={setSelectedFilterByCountry}
+        />
+      </div>
+
+      <div className="flex flex-col gap-5 justify-center items-center my-6 px-3 md:max-w-xl max-w-md w-full">
+        {filteredEvents.map((event, index) => (
+          <EventWagerCardComponent event={event} key={index} />
+        ))}
+      </div>
+    </Fragment>
+  );
+};
+
+const LobbySlideComponent = () => {
+  const [selectedFilter, setSelectedFilter] = useState("all");
+  const [wagers, setWagers] = useState<WagerType[]>(wagerList);
+  const [filteredWagers, setFilteredWagers] = useState<WagerType[]>([]);
+
+  useEffect(() => {
+    if (selectedFilter === "all") setFilteredWagers(wagers);
+    else
+      setFilteredWagers(
+        wagers.filter((wager) => wager.event.sport.name === selectedFilter)
+      );
+  }, [selectedFilter]);
+  return (
+    <Fragment>
+      <div className="md:max-w-2xl max-w-md w-full mt-2 pb-4">
+        <FilterMenuComponent
+          tabs={sportsList}
+          selectedTab={selectedFilter}
+          setSelectedTab={setSelectedFilter}
+        />
+      </div>
+
+      <div className="flex flex-col gap-5 justify-center items-center my-6 px-3 md:max-w-xl max-w-md w-full">
+        {filteredWagers.map((wager, index) => (
+          <WagerCardComponent wager={wager} key={index} />
+        ))}
+      </div>
+    </Fragment>
+  );
+};
 
 const WagerPage = () => {
+  const [swiper, setSwiper] = useState<SwiperClass | null>(null);
+  const [selectedTab, setSelectedTab] = useState("events");
+
+  const [isEnd, setIsEnd] = useState(false);
+
+  console.log(swiper);
+
+  useEffect(() => {
+    setSelectedTab(isEnd ? "lobby" : "events");
+  }, [isEnd]);
+
   return (
     <div className="mt-20 flex flex-col w-full px-5 justify-center items-center">
       <div className="w-full flex justify-end items-center p-1">
@@ -28,9 +116,65 @@ const WagerPage = () => {
         </svg>
       </div>
 
-      <div className="flex flex-col gap-6 mt-5">
-        <div></div>
+      <div className="flex flex-col gap-6 my-5">
+        {/* tabs */}
+        <div className="flex select-none justify-center items-center my-2">
+          <div className="flex justify-between relative gap-28 px-8 items-center bg-white rounded-full">
+            <div
+              className={`absolute py-4 w-[46%] transition-all bg-darkGold rounded-full h-full ${
+                selectedTab === "lobby" ? "left-[56.5%]" : "left-0"
+              }`}
+            ></div>
+            <button
+              type="button"
+              onClick={() => {
+                setSelectedTab("events");
+                swiper?.slidePrev();
+              }}
+              className={`z-10 text-lg font-medium cursor-pointer h-full ${
+                selectedTab === "events" ? "text-slate-200" : "text-slate-800"
+              }`}
+            >
+              Events
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                setSelectedTab("lobby");
+                swiper?.slideNext();
+              }}
+              className={`z-10 text-lg py-4 font-medium cursor-pointer h-full ${
+                selectedTab === "lobby" ? "text-slate-200" : "text-slate-800"
+              }`}
+            >
+              Lobby
+            </button>
+          </div>
+        </div>
       </div>
+
+      <Swiper
+        onSwiper={setSwiper}
+        onSlideChange={(swiper) => setIsEnd(swiper.isEnd)}
+        className="flex gap-4 justify-center w-full overflow-hidden relative mx-auto"
+        slidesPerView={1}
+        spaceBetween={30}
+        scrollbar={{
+          hide: true,
+        }}
+        keyboard={{
+          enabled: true,
+        }}
+        navigation={true}
+        modules={[Scrollbar, Navigation, Keyboard, Pagination]}
+      >
+        <SwiperSlide className="py-2 !flex flex-col justify-center items-center">
+          <EventsSlideComponent />
+        </SwiperSlide>
+        <SwiperSlide className="py-2 !flex flex-col justify-center items-center">
+          <LobbySlideComponent />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
